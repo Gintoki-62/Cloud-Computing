d27754d92b55ef033fa528431f1eede21de7c6f2
wenzhen-3434 <wzgoh-pm23@student.tarc.edu.my>
2025-04-15 21:50:34 +0800

    .

---
+++ add-product.php
@@ -0,0 +1,40 @@
+<?php
+include 'header.php';
+?>
+<?php
+?>
+
+
+<!-- breadcrumb-section -->
+<div class="breadcrumb-section breadcrumb-bg">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 offset-lg-2 text-center">
+					<div class="breadcrumb-text">
+						<p>Product</p>
+						<h1>Shopping Cart</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+    <!-- cart -->
+	<div class="cart-section mt-150 mb-150">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="cart-table-wrap">
+						<table class="cart-table">
+							<thead class="cart-table-head">
+								<tr class="table-head-row">
+									<th class="product-remove">No.</th>
+									<th class="product-image">Product</th>
+									<th class="product-name">Name</th>
+									<th class="product-price">Price</th>
+									<th class="product-quantity">Quantity</th>
+									<th class="product-total">Total</th>
+									<th class="product-total"></th>
+									<th class="product-total"></th>
+								</tr>
+							</thead>
