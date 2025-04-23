f0ebd3314ce2efc1e13af8e6a6a9050e7edefab4
Angel <tanangel2709@gmail.com>
2025-04-23 14:58:03 +0800

    hh

--- homepage.php
+++ homepage.php
@@ -1,5 +1,5 @@
 <?php
-include 'config.php';
+include '.vscode/config.php'; 
 ?>
 <!DOCTYPE html>
 <html lang="en">
--- login.php
+++ login.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $user = $result->fetch_assoc();
             
             if (password_verify($password, $user['password'])) {
-                $_SESSION['user_id'] = $user['id'];
+                $_SESSION['user_id'] = $user['user_id'];
                 $_SESSION['username'] = $user['username'];
 
                 header("Location: homepage.php");
